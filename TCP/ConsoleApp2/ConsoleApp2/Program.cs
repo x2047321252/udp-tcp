d8662af6a8244597673efdc3c851c891a15fe314
x2047321252 <84715459+x2047321252@users.noreply.github.com>
2021-11-24 17:38:55 +0800

    Add files via upload

---
+++ TCP/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            //连接准备
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 2000);
+            //创建端口对象
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(iPEndPoint);//绑定端口
+
+            //循环监听并处理消息
+            while (true)
+            {
+                i++;
+                try
+                {
+                    Console.WriteLine("Perform operations {0} :", i);
+                    Console.WriteLine("************************************");
+                    socket.Listen(0);//开始监听
+                    Console.WriteLine("等待连接...");
+
+                    //实例化一个新的端口
+                    Socket temp = socket.Accept();//为新建连接创建新的Socket
+                    Console.WriteLine("连接成功！");
+
+                    //接收客户端发来的消息
+                    string recvStr = "";
+                    byte[] recvBytes = new byte[1024];
+                    int bytes;
+                    bytes = temp.Receive(recvBytes, recvBytes.Length, 0);//接收客户端信息
+                    recvStr += Encoding.UTF8.GetString(recvBytes, 0, bytes);
+                    Console.WriteLine("消息内容:{0}", recvStr);//显示客户端传来的信息
+
+                    //返回给客户端连接成功的信息
+                    string sendmessage = "成功发送消息，客户端！";
+                    byte[] bs = Encoding.UTF8.GetBytes(sendmessage);
+                    temp.Send(bs, bs.Length, 0);
+
+                    //关闭端口
+                    temp.Close();
+                    Console.WriteLine("Completed...");
+                    Console.WriteLine("-----------------------------------");
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine("ArgumentNullException: {0}", e);
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("SocketException: {0}", e);
+                }
+            }
+        }
+    }
+}
---
+++ TCP/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str;
+                //做连接准备,ip地址为服务器ip地址
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("192.168.43.236"), 8080);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                //开始连接
+                str = "连接服务器中...";
+                textBox1.AppendText(str + Environment.NewLine);
+                socket.Connect(iPEndPoint);//连接到服务器
+
+                //发送消息
+                string sendmessage = textBox2.Text;
+                str = "消息内容: " + sendmessage;
+                textBox1.AppendText(str + Environment.NewLine);
+                byte[] bs = Encoding.UTF8.GetBytes(sendmessage);
+                str = "正在发送给服务器...";
+                textBox1.AppendText(str + Environment.NewLine);
+                socket.Send(bs, bs.Length, 0);//发送信息
+
+                //接收服务器端反馈信息
+                string recvStr = "";
+                byte[] recvBytes = new byte[1024];
+                int bytes;
+                bytes = socket.Receive(recvBytes, recvBytes.Length, 0);//接收服务器端返回的信息
+                recvStr += Encoding.UTF8.GetString(recvBytes, 0, bytes);
+                str = "服务器返回信息：" + recvStr;
+                textBox1.AppendText(str + Environment.NewLine);
+
+                //关闭socket
+                socket.Close();
+            }
+            //异常处理
+            catch (ArgumentNullException f)
+            {
+                string str = "ArgumentNullException: " + f.ToString();
+                textBox1.AppendText(str + Environment.NewLine);
+            }
+            catch (SocketException f)
+            {
+                string str = "ArgumentNullException: " + f.ToString();
+                textBox1.AppendText(str + Environment.NewLine);
+            }
+            textBox1.AppendText("" + Environment.NewLine);
+            textBox2.Text = "";
+        }
+    }
+}
